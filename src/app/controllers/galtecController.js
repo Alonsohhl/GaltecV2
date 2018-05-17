52e2c48287363802f128fad78a02cea58be9c3a6
Alonso <alonso.hl25@gmail.com>
2018-05-17 11:19:19 -0500

    primer commit

---
+++ src/app/controllers/galtecController.js
@@ -0,0 +1,13 @@
+const controller = {};
+
+//nodemailer = require('nodemailer');
+
+controller.index2 = (req, res) => {
+
+     
+     res.render('galtec/index');
+     console.log('Carga completa');
+
+};
+  
+module.exports = controller;
---
+++ src/app/routes/galtecRoutes.js
@@ -0,0 +1,16 @@
+
+const router = require('express').Router();
+
+const galtecController = require('../controllers/galtecController');
+
+router.get('/', galtecController.index2);
+//router.post('/enviar', PanamController.enviar);
+//router.get('/exito', PanamController.exito);
+//router.get('/denuncia', PanamController.denuncia);
+//router.post('/enviar_denuncia', PanamController.enviar_denuncia);
+/*router.post('/add', customerController.save);
+router.get('/update/:id', customerController.edit);
+router.post('/update/:id', customerController.update);
+router.get('/delete/:id', customerController.delete);
+*/
+module.exports = router;
