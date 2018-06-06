f6df01549aee3bf6a63e1a7fd78a00cf71fa15ad
Alonso <alonso.hl25@gmail.com>
2018-06-06 12:27:24 -0500

    envio de correos

--- src/app/controllers/galtecController.js
+++ src/app/controllers/galtecController.js
@@ -1,6 +1,6 @@
 const controller = {};
 
-//nodemailer = require('nodemailer');
+nodemailer = require('nodemailer');
 
 controller.index2 = (req, res) => {
 
@@ -33,5 +33,59 @@ controller.servicios = (req, res) => {
     console.log('servicios cargando');
 
 };
+
+
+controller.enviar_msg = (req, res) => {
+    output =`
+       <p> Sugerencia o queja </p>
+       <h3>=== Detalles ===</h3>
+       <ul>
+         <li> Nombre: ${req.body.name}</li>
+         <li> Nombre: ${req.body.subject}</li>
+         <li> Email: ${req.body.email}</li>
+         <li> Dirección: ${req.body.budget}</li>
+        
+   
+         
+       </ul> 
+       `;
+   
+       let transporter = nodemailer.createTransport({
+         host: 'smtp.gmail.com',
+         port: 587,//25,//587,
+         secure: false, // true for 465, false for other ports
+         auth: {
+             user: 'galtec.inf@gmail.com', // generated ethereal user
+             pass: 'alonsoucsm'  // generated ethereal password
+         },
+         tls:{
+           rejectUnauthorized:false
+         }
+       });
+   
+       let mailOptions = {
+           from: '"Galtec Mailer Contacto" <alonso.hhl@galtec.org>', // sender address
+           to: 'alonso.hhl@galtec.org',// list of receivers
+           subject: 'recepcion de sugerencias', // Subject line
+           text: 'Revise el correo en un navegador valido, compatible con HTML', // plain text body
+           html: output // html body ,
+           
+       };
+   
+       // send mail with defined transport object
+         transporter.sendMail(mailOptions, (error, info) => {
+           if (error) {
+               return console.log(error);
+           }
+           console.log('Message sent: %s', info.messageId);   
+           console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+           
+           res.render('galtec/index');
+   //        res.render('form/forms', {msg:'1'});
+       });
+       //res.render('form/exito');
+       console.log(output);
+     };
+   
   
 module.exports = controller;
--- src/app/routes/galtecRoutes.js
+++ src/app/routes/galtecRoutes.js
@@ -8,6 +8,7 @@ router.get('/test', galtecController.test);
 router.get('/contacto', galtecController.contacto);
 router.get('/nosotros', galtecController.nosotros);
 router.get('/servicios', galtecController.servicios);
+router.post('/enviar_msg', galtecController.enviar_msg);
 //router.post('/enviar', PanamController.enviar);
 //router.get('/exito', PanamController.exito);
 //router.get('/denuncia', PanamController.denuncia);
